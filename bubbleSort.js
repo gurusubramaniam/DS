c8062e7445f6ca34203669a5902c9a860fee77b0
Gurusubramaniam <gurutii@gmail.com>
2023-02-07 09:17:59 -0800

    bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  var sort;
+  do {
+    sort = false
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        sort = true;
+      }
+    }
+  } while (sort)
+  return arr
+}
+
+console.log(bubbleSort([-6, 10, 8, -2, 4]))
