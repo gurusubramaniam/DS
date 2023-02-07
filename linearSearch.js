df740d8c4a85a944feebcc44386061c7f2153270
Gurusubramaniam <gurutii@gmail.com>
2023-02-07 13:43:58 -0800

    Update linearSearch.js

--- linearSearch.js
+++ linearSearch.js
@@ -1,3 +1,8 @@
+/**
+In computer science, a linear search or sequential search is a method for finding an element within a list. 
+It sequentially checks each element of the list until a match is found or the whole list has been searched.
+**/
+
 function linearSearch(target) {
   const arr = [-5, 2, 10, 4, 6]
   for (i = 0; i < arr.length; i++) {
