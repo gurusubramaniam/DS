90ea98acfcde4c085a24bb5243735496c29268a9
Gurusubramaniam <gurutii@gmail.com>
2023-10-08 08:50:03 -0700

    Time: 63 ms (96.52%), Space: 45.9 MB (96.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let left=0, right=0; 
-    while(left < nums.length){
-        // Swapping the non zero element to hr left of the given Array. 
-        if(nums[left] !== 0) {
-            [nums[left],nums[right]] = [nums[right], nums[left]];
-            right ++ 
+    // let left=0, right=0; 
+    // while(left < nums.length){
+    //     // Swapping the non zero element to hr left of the given Array. 
+    //     if(nums[left] !== 0) {
+    //         [nums[left],nums[right]] = [nums[right], nums[left]];
+    //         right ++ 
+    //     }
+    //     left ++
+    // }
+    let left=0; 
+    for(let i=0; i< nums.length; i++) {
+        if(nums[i]!==0) {
+            [nums[left], nums[i]] =[nums[i], nums[left]]
+            left ++
         }
-        left ++
     }
 };
